b38ce3492b7d977c203eaad3116f607f795eda49
cbell2 <cbell2@wpi.edu>
2020-03-07 22:37:40 -0500

    Descent stuff

--- groupNN/testcharacter.py
+++ groupNN/testcharacter.py
@@ -14,6 +14,7 @@ from colorama import Fore, Back
 
 # import numpy
 
+
 class TestCharacter(CharacterEntity):
 
     def do(self, wrld):
@@ -23,32 +24,67 @@ class TestCharacter(CharacterEntity):
         # ===============================
         # Initializes all variables and sets up world for testing
 
+        weights_file = open("/Users/chrisbell/Dropbox/SeniorYear/CTerm/CS 4341/Projects/Bomberman3/groupNN/weights.txt", "r")
+
         # The rate at which to change the weights
         learning_rate = 0.2
 
-        # TODO All weights retrieved from .txt file to hold data between games
+        # All weights retrieved from .txt file to hold data between games
 
         # The weight applied to the exit feature
-        exit_weight = -200
+        exit_weight = int(weights_file.readline())
 
         # The weight applied to the monster feature
-        monster_weight = 4
+        monster_weight = int(weights_file.readline())
         
         # The weight applied to the bomb feature
-        bomb_weight = 2
+        bomb_weight = int(weights_file.readline())
         
         # The weight applied to the wall feature
-        wall_weight = 1
+        wall_weight = int(weights_file.readline())
+
+        # TODO Close after adjusting weights
+        weights_file.close()
+
+        print("\nUsing weights:")
+        print("  exit: ", exit_weight, " monster: ", monster_weight, " bomb: ", bomb_weight,
+              " wall: ", wall_weight)
+
+        # TODO store in descent.txt
+        # Gradient Descent percentage variable to decrease by set amount
+
+        desc_file = open("/Users/chrisbell/Dropbox/SeniorYear/CTerm/CS 4341/Projects/Bomberman3/groupNN/descent.txt", "r+")
+
+        grad_desc_perc = 10000
+
+        # Amount to decrease the percentage by
+        dec_amt = 1
+
+        # Current percentage chance
+        curr_desc = desc_file.readline()
+
+        print("OLD VALUE: ", curr_desc)
+
+        int_curr = int(curr_desc)
+        new_total = int_curr + dec_amt
+        desc_file.seek(0)
+        desc_file.truncate()
+        desc_file.write(str(new_total))
+        # desc_file.seek(0)
+        # print("NEW VALUE: ", desc_file.readline())
+        desc_file.close()
 
         # Choose based on decreasing percentage random or best given moves
         # TODO Gradient Descent
         # TODO decreasing amount and amt of turns is retrieved from .txt file
 
-        # Makes a random action
-        # self.non_rand_action(wrld, exit_weight, bomb_weight, monster_weight, wall_weight)
+        rand_choice = random.randrange(0, 10000, 1)
 
-        # Makes a non-random action
-        self.random_action(wrld)
+        if rand_choice <= int_curr:
+            self.non_rand_action(wrld, exit_weight, bomb_weight, monster_weight, wall_weight)
+
+        else:
+            self.random_action(wrld)
 
         pass
 
@@ -62,6 +98,7 @@ class TestCharacter(CharacterEntity):
 
     def non_rand_action(self, wrld, exit_weight, bomb_weight, monster_weight, wall_weight):
         # Variable to hold the greatest value found when moving
+        # max_action_value = float("-inf")
         max_action_value = float("-inf")
         max_action = (1, 0)
 
