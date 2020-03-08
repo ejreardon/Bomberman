0c874a80a498bd52ce956d26ead09a3e2a0bb34f
Eric Reardon <ericjreardon@gmail.com>
2020-03-08 11:30:02 -0400

    initial merge

--- groupNN/testcharacter.py
+++ groupNN/testcharacter.py
@@ -129,23 +129,93 @@ class TestCharacter(CharacterEntity):
         p_bomb = f.distance_to_bomb(self.x, self.y, wrld)
         p_wall = f.next_to_wall(self.x, self.y, wrld)
 
+        print("current coordinates: ", (self.x, self.y))
+
         # Loop through all 8 moves using the copied char and decide which one to make
         # Loop through all the dx values
-        for dx in range (-1, 2):
+        for dx in range(-1, 2):
             # Loop through all the dy values
-            for dy in range (-1, 2):
-                print("=================================")
-                print("SPACE (", dx, dy, ")")
-                
-                move_val = f.distance_to_exit(x+dx, y+dy, wrld) * exit_weight + f.distance_to_monster(x+dx, y+dy, wrld) * monster_weight + \
-                f.distance_to_bomb(x+dx, y+dy, wrld) * bomb_weight + f.next_to_exit(x+dx, y+dy, wrld) * exit_weight + \
-                f.next_to_monster(x+dx, y+dy, wrld) * monster_weight + f.next_to_wall(x+dx, y+dy, wrld) * wall_weight + \
-                f.is_in_explosion(x+dx, y+dy, wrld) * bomb_weight
-                if move_val > max_action_value:
+            for dy in range(-1, 2):
+                # Reset world to OG position and get the character from it
+                # copied_world = SensedWorld.from_world(wrld)
+                # copied_char = copied_world.me(self)
+
+                x = self.x + dx
+                y = self.y + dy
+                out_bounds_x = x < 0 or x >= wrld.width()
+                out_bounds_y = y < 0 or y >= wrld.height()
+
+                # ignore moves that are not possible
+                if out_bounds_x:
+                    continue
+                if out_bounds_y:
+                    continue
+                if self.x == x and self.y == y:
+                    continue
+                if wrld.wall_at(x, y):
+                    continue
+                if wrld.exit_at(x, y):
+                    self.move(dx, dy)
+                    break
+
+                print("================================================")
+                print("TODO: CHECK (", dx, dy, ")")
+                copied_char.move(dx, dy)
+                # potentially update copied world and character?
+                # fixes same values for each move but distorts sense of self coordinates
+                copied_world, events = copied_world.next()
+                copied_char = copied_world.me(self)
+
+                # if the move causes the char to die, do not move there!
+                if copied_char is None:
+                    continue
+
+                print("new character coordinates: ", (copied_char.x, copied_char.y))
+
+                # evaluation of move value for each adjacent move (+2 to each due to -1 return for some - avoid 1/0)
+                exit_val = 1 / (f.distance_to_exit(copied_char, copied_world)) * exit_weight
+                monst_val = -1 / (f.distance_to_monster(copied_char, copied_world)) * monster_weight
+                bomb_val = -1 / (1 + f.distance_to_bomb(copied_char, copied_world)) * bomb_weight
+                adjacent_exit_val = f.next_to_exit(copied_char, copied_world) * adjacent_exit_weight
+                adjacent_monst_val = -f.next_to_monster(copied_char, copied_world) * adjacent_monst_weight
+                # wall_val = 1/(1+f.next_to_wall(copied_char, copied_world)) * wall_weight
+                print(f.is_in_explosion(copied_char, copied_world))
+                explosion_val = 1 / (1 + f.is_in_explosion(copied_char, copied_world)) * explosion_weight
+
+                print("=======================")
+                print("exit value: ", 1 / f.distance_to_exit(copied_char, copied_world) * exit_weight)
+                print("monster value: ", -1 / (f.distance_to_monster(copied_char, copied_world)) * monster_weight)
+                print("bomb value: ", -1 / (1 + f.distance_to_bomb(copied_char, copied_world) * bomb_weight))
+                print("next to exit value: ", f.next_to_exit(copied_char, copied_world) * adjacent_exit_weight)
+                print("next to monster value: ", -f.next_to_monster(copied_char, copied_world) * adjacent_monst_weight)
+                print("next to wall value: ", 1 / (1 + f.next_to_wall(copied_char, copied_world)) * wall_weight)
+                print("in explosion value: ", 1 / (1 + f.is_in_explosion(copied_char, copied_world)) * explosion_weight)
+                print("=======================")
+
+                move_val = exit_val + monst_val + bomb_val + adjacent_exit_val + adjacent_monst_val + explosion_val  # + wall_val
+                print("total value: ", move_val)
+
+                # store the greatest value move
+                if move_val >= max_action_value:
                     max_action_value = move_val
-                    max_action = (dx, dy)
-                print("val: ", move_val)
-                print("=================================")
+                    max_action[0] = dx
+                    max_action[1] = dy
+
+        print("best value: ", max_action_value)
+        print("x: ", max_action[0])
+        print("y: ", max_action[1])
+
+        if f.next_to_wall(self, wrld):
+            self.place_bomb()
+        if f.next_to_exit(self, wrld):
+            for x in range(self.x - 1, self.x + 2):
+                for y in range(self.y - 1, self.y + 2):
+                    if 0 <= x < wrld.width() and 0 <= y < wrld.height():
+                        if wrld.exit_at(x, y):
+                            self.move(x, y)
+                            continue
+        else:
+            self.move(max_action[0], max_action[1])
 
         if make_move == True:
             print("Making move (", max_action[0], ",", max_action[1], ") with value ", max_action_value)
